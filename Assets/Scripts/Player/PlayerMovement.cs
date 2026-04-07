79257aafde2190c717ca28818e4e9762e66f7dcb
ChristFollw3r <nilbadia@gmail.com>
2026-04-07 11:13:23 +0200

    Restricted player jump.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Collider2D playerCollider;
     private InputSystem_Actions playerInput;
-    [SerializeField] private float speed;
 
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();   
+        playerCollider = GetComponent<Collider2D>();
         playerInput = new InputSystem_Actions();
         playerInput.Enable();
     }
@@ -24,6 +27,10 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 movement = playerInput.Player.Move.ReadValue<Vector2>();
         rb.linearVelocityX = movement.x * speed;
+
+        Vector2 origin = new Vector3(transform.position.x, playerCollider.bounds.min.y); // Dude still gets stuck on the wall
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.2f);
+        if (playerInput.Player.Jump.WasPerformedThisFrame() && hit.collider != null) rb.linearVelocityY = jumpForce;
     }
    
 }
