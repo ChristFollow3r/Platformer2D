3ed84fcccee24a34c80290671a637bd3ecdcce68
ChristFollw3r <nilbadia@gmail.com>
2026-04-07 13:17:48 +0200

    Uninstalled visual community and installed Rider and cleaned up PlayerMovement script.

---
+++ Assets/Scripts/Player/BreakAndPlace.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class BreakAndPlace : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+        [SerializeField] private int reachDistance;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,36 +1,39 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player // Rider yellow underlying was telling me to use a namespace
 {
-    private Rigidbody2D rb;
-    private Collider2D playerCollider;
-    private InputSystem_Actions playerInput;
-
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpForce;
-    private void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();   
-        playerCollider = GetComponent<Collider2D>();
-        playerInput = new InputSystem_Actions();
-        playerInput.Enable();
-    }
-    private void Update()
-    {
-        if (rb.linearVelocityY < 0) rb.gravityScale = 5f;
-        else rb.gravityScale = 3f;
-        Movemenet();
-    }
+        private Rigidbody2D rb;
+        private Collider2D playerCollider;
+        private InputSystem_Actions playerInput;
 
-    private void Movemenet()
-    {
-        Vector2 movement = playerInput.Player.Move.ReadValue<Vector2>();
-        rb.linearVelocityX = movement.x * speed;
+        [SerializeField] private float speed;
+        [SerializeField] private float jumpForce;
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();   
+            playerCollider = GetComponent<Collider2D>();
+            playerInput = new InputSystem_Actions();
+            playerInput.Enable();
+        }
+        private void Update()
+        {
+            rb.gravityScale = rb.linearVelocityY < 0 ? 5f : 3f;
+            Movement();
+        }
 
-        Vector2 origin = new Vector3(transform.position.x, playerCollider.bounds.min.y); // Dude still gets stuck on the wall
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.2f);
-        if (playerInput.Player.Jump.WasPerformedThisFrame() && hit.collider != null) rb.linearVelocityY = jumpForce;
-    }
+        private void Movement()
+        {
+            var movement = playerInput.Player.Move.ReadValue<Vector2>();
+            rb.linearVelocityX = movement.x * speed;
+
+            Vector2 origin = new Vector3(transform.position.x, playerCollider.bounds.min.y); // Dude still gets stuck on the wall
+            var hit = Physics2D.Raycast(origin, Vector2.down, 0.2f);
+            if (playerInput.Player.Jump.WasPerformedThisFrame() && hit.collider is not null) rb.linearVelocityY = jumpForce; // I uninstalled visual community and installed
+            // rider cause community was working so bad, and I'm done with that IDE. Rider warns me about bad performance code, that's why I wrote is not null instead of != null.
+        }
    
+    }
 }
+
