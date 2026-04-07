46ce2ed2c5f4a991a0cbb621f70f9909d1f749bf
ChristFollw3r <nilbadia@gmail.com>
2026-04-07 10:56:49 +0200

    Added rb movemenet.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private InputSystem_Actions playerInput;
+    [SerializeField] private float speed;
 
-    void Update()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();   
+        playerInput = new InputSystem_Actions();
+        playerInput.Enable();
+    }
+    private void Update()
+    {
+        if (rb.linearVelocityY < 0) rb.gravityScale = 5f;
+        else rb.gravityScale = 3f;
+        Movemenet();
+    }
 
+    private void Movemenet()
+    {
+        Vector2 movement = playerInput.Player.Move.ReadValue<Vector2>();
+        rb.linearVelocityX = movement.x * speed;
     }
    
 }
