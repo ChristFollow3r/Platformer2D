7759284a236f21a0aeacec489ab2c56e5cf6c431
ChristFollw3r <nilbadia@gmail.com>
2026-04-08 20:03:18 +0200

    Added a 2D material so that the player doesnt get stuck to the walls when running.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -30,8 +30,7 @@ namespace Player // Rider yellow underlying was telling me to use a namespace
 
             Vector2 origin = new Vector3(transform.position.x, playerCollider.bounds.min.y); // Dude still gets stuck on the wall
             var hit = Physics2D.Raycast(origin, Vector2.down, 0.2f);
-            if (playerInput.Player.Jump.WasPerformedThisFrame() && hit.collider is not null) rb.linearVelocityY = jumpForce; // I uninstalled visual community and installed
-            // rider cause community was working so bad, and I'm done with that IDE. Rider warns me about bad performance code, that's why I wrote is not null instead of != null.
+            if (playerInput.Player.Jump.WasPerformedThisFrame() && hit.collider is not null) rb.linearVelocityY = jumpForce; 
         }
    
     }
