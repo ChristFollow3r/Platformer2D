af903c4b07a26e5675d867d3c6969cc8c60f034d
ChristFollw3r <nilbadia@gmail.com>
2026-04-15 19:52:22 +0200

    Added tmp pack, completly unecessary for now. Started again with the inventory stuff cause the last script made no sense at all

---
+++ Assets/Scripts/Data/InventorySlot.cs
@@ -0,0 +1,35 @@
+    using UnityEngine;
+    using Scriptable_Objects_Scripts;
+
+    public class InventorySlot
+    {
+        public Item item;
+        public int amount = 0;
+
+        public bool IsEmpty => item is null || amount <= 0;
+        public bool IsFull => item is not null && amount >= item.maxStack;
+
+        public bool CanBeStacked(Item otherItem) => item is not null && otherItem is not null && item == otherItem;
+
+        public int AddItem(Item newItem, int amountToAdd)
+        {
+            if (IsEmpty)
+            {
+                item = newItem;
+                amount = Mathf.Min(amountToAdd, item.maxStack); // Returns the smallest number
+                return amountToAdd - amount;
+            }
+
+            if (CanBeStacked(newItem))
+            {
+                int spaceLeft = item.maxStack - amount;
+                int newAmountToAdd = Mathf.Min(spaceLeft, amountToAdd);
+                
+                amount += newAmountToAdd;
+                return amountToAdd - newAmountToAdd;
+            }
+                
+            return amountToAdd;
+        }
+        
+    }
