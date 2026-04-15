ce4d21b004d4fc6e9556792c2ce2545e3d5d6d04
ChristFollw3r <nilbadia@gmail.com>
2026-04-15 22:26:33 +0200

    Started inventory class

--- Assets/Scripts/Data/Inventory.cs
+++ Assets/Scripts/Data/Inventory.cs
@@ -1,7 +1,26 @@
-using System;
-using System.Collections.Generic;
 
-public class Inventory {
+public class Inventory
+{
+    private Data.InventorySlot[,] slots =  new Data.InventorySlot[3, 9];
+    private Data.InventorySlot[] hotBarSlots = new Data.InventorySlot[9];
 
+    public Inventory()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                slots[i, j] = new Data.InventorySlot();
+            }
+        }
 
+        for (int i = 0; i < 9; i++)
+        {
+            hotBarSlots[i] = new Data.InventorySlot();
+        }
+    }
+    
+    // Add global inventory method (loop through arrays and call AddItem() when needed
+    
+    // Add two getters for the arrays
 }
