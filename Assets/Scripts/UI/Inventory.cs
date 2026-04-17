561a0d87e78d254b93cb6c2fd69cbdf714b7cf12
ChristFollw3r <nilbadia@gmail.com>
2026-04-17 21:02:26 +0200

    Got the hotbar UI working!

--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -8,7 +8,7 @@ namespace UI
         [SerializeField] private GameObject hotbar;
         [SerializeField] private Player.PlayerManager playerManager;
 
-        private void Start()
+        private void Awake()
         {
             foreach (var slot in playerManager.Inventory.GetHotBarSlots())
                 slot.OnSlotChanged += UpdateHotbarUI;
@@ -17,15 +17,19 @@ namespace UI
         private void UpdateHotbarUI()
         {
             var hotBarSlots = playerManager.Inventory.GetHotBarSlots();
-            
+            Debug.Log("I'm being called");
             for (int i = 0; i < 9; i++)
             {
-                Debug.Log("Hotbar Slot: " + hotBarSlots[i] +  "being filled");
-                if (hotBarSlots[i] is null) continue; // This is unnecessary ?
-                Transform hotBarSlot = hotbar.transform.GetChild(i);
-                if (hotBarSlots[i].GetItem() is null) continue;
+                Debug.Log("Hotbar Slot: " + i +  "being filled");
+                if (hotBarSlots[i].IsEmpty)
+                {
+                    hotbar.transform.GetChild(i).GetChild(0).GetComponent<Image>().enabled = false;
+                    continue;
+                }
+                
+                hotbar.transform.GetChild(i).GetChild(0).GetComponent<Image>().enabled = true;
                 Sprite itemSprite = hotBarSlots[i].GetItem().itemIcon;
-                hotBarSlot.GetChild(0).GetComponent<Image>().sprite = itemSprite;
+                hotbar.transform.GetChild(i).GetChild(0).GetComponent<Image>().sprite = itemSprite;
             }
         }
     }
