25928edb8dfb81bc9d916cd14562d25dd1efa195
ChristFollw3r <nilbadia@gmail.com>
2026-04-20 10:22:02 +0200

    Added walljump with the help of IA

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,56 +8,72 @@
             private Collider2D playerCollider;
             private Animator playerAnimator;
             
-            bool canDoulbeJump = false;
             
             public InputSystem_Actions playerInput;
 
+            [Header("Movement settings")]
             [SerializeField] private float speed;
             [SerializeField] private float jumpForce;
+            [SerializeField] private Vector2 wallJumpForce = new Vector2(7f, 12f);
+
+            private bool canDoubleJump;
+            private float wallJumpTime = 0.25f;
+            
             private void Awake()
             {
                 rb = GetComponent<Rigidbody2D>();   
                 playerCollider = GetComponent<Collider2D>();
-                playerAnimator = GetComponent<Animator>();
+                // playerAnimator = GetComponent<Animator>();
                 playerInput = new InputSystem_Actions();
                 playerInput.Enable();
             }
             private void Update()
             {
-                rb.gravityScale = rb.linearVelocityY < 0 ? 5f : 3f;
-                Movement();
+                bool isGrounded = Physics2D.Raycast(playerCollider.bounds.center, Vector2.down, playerCollider.bounds.extents.y + 0.2f).collider is not null;
+                bool isTouchingLeftWall = Physics2D.Raycast(playerCollider.bounds.center, Vector2.left, playerCollider.bounds.extents.x + 0.2f).collider is not null;
+                bool isTouchingRightWall = Physics2D.Raycast(playerCollider.bounds.center, Vector2.right, playerCollider.bounds.extents.x + 0.2f).collider is not null;
+                
+                Movement(isGrounded, isTouchingLeftWall, isTouchingRightWall);
             }
 
-            private void Movement()
+            private void Movement(bool isGrounded, bool isTouchingLeftWall, bool isTouchingRightWall)
             {
-                var movement = playerInput.Player.Move.ReadValue<Vector2>();
-                rb.linearVelocityX = movement.x * speed;
+                float movement = playerInput.Player.Move.ReadValue<Vector2>().x;
                 
-                Vector2 origin = new Vector3(transform.position.x, playerCollider.bounds.min.y); // Dude still gets stuck on the wall
-                var hit = Physics2D.Raycast(origin, Vector2.down, 0.2f);
+                if (isGrounded) canDoubleJump = true;
                 
-                bool isGrounded = hit.collider is not null;
-                if (isGrounded) canDoulbeJump = true;
+                if (isTouchingLeftWall || isTouchingRightWall && !isGrounded)
+                    rb.gravityScale = 1.5f;
+                else rb.gravityScale = rb.linearVelocityY < 0 ? 5f : 3f;
+                
+                if (wallJumpTime > 0f)
+                    wallJumpTime -= Time.deltaTime;
+                else
+                    rb.linearVelocityX = movement * speed;
 
                 if (playerInput.Player.Jump.WasPerformedThisFrame())
                 {
-                    if (isGrounded)
+                    if (!isGrounded && (isTouchingLeftWall || isTouchingRightWall))
+                    {
+                        float direction = isTouchingLeftWall ?  1 : -1;
+                        rb.linearVelocity = new Vector2(direction * wallJumpForce.x, wallJumpForce.y);
+                        
+                        wallJumpTime = 0.25f;
+                        canDoubleJump = true;
+                    }
+                    
+                    else if (isGrounded)
                         rb.linearVelocityY = jumpForce;
                     
-                    else if (canDoulbeJump)
+                    else if (canDoubleJump)
                     {
                         rb.linearVelocityY = jumpForce;
-                        canDoulbeJump = false;
+                        canDoubleJump = false;
                     }
                 }
                 
             }
-
-            private void WallJump(bool isGrounded)
-            {
-                var leftHit = Physics2D.Raycast(playerCollider.bounds.min, Vector2.left, 0.2f);
-                var rightHit = Physics2D.Raycast(playerCollider.bounds.min, Vector2.right, 0.2f);
-            }
+            
             
         }
     }
