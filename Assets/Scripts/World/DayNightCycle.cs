6148148939463b062d1995b055a72e94a8a28377
ChristFollw3r <nilbadia@gmail.com>
2026-06-06 23:17:07 +0200

    Made fog work on daynight cycle

--- Assets/Scripts/World/DayNightCycle.cs
+++ Assets/Scripts/World/DayNightCycle.cs
@@ -8,6 +8,8 @@ namespace World
         [SerializeField] private Light2D globalLight;
         [SerializeField] private Gradient dayNightGradient;
         [SerializeField] private float dayNightDuration;
+        [SerializeField] private Material fogMaterial;
+        [SerializeField] private AnimationCurve fogOpacityCurve;
 
         private float time;
         public float CurrentTime => time;
@@ -17,7 +19,14 @@ namespace World
         {
             time += Time.deltaTime / dayNightDuration;
             if (time > 1) time = 0;
+
             globalLight.color = dayNightGradient.Evaluate(time);
+
+            if (fogMaterial != null)
+            {
+                float targetOpacity = fogOpacityCurve.Evaluate(time);
+                fogMaterial.SetFloat("_FogOpacity", targetOpacity);
+            }
         }
     }
 }
